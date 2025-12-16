5698b0fbb4d5babb04655be2633661dd28a90587
Elfar Oliver Sigurðarson <eos35@hi.is>
2025-12-16 15:58:02 +0000

    Decided the brick is a multiplier

--- dice/redDice.cs
+++ dice/redDice.cs
@@ -6,10 +6,26 @@ namespace PracticeCsharp
   {
     public int RedDiceRoll()
     {
-      int[] redDice = [2, 2, 3, 4, 5, 6];
+      int[] redDice = [1, 2, 3, 4, 5, 6];
       Random random = new Random();
       return redDice[random.Next(0, redDice.Length)];
+
+      //return random.Next(1, 7);
     }
+
+    /**
+    Adds to damage based on the value of the red dice roll
+    If roll is 1, add 10% to damage
+    If roll is 2, add 20% to damage
+    etc...
+    **/
+    public double AddToDamage(double baseDamage)
+{
+    int roll = RedDiceRoll();   // 1–6
+    double multiplier = roll * 0.10; // 10% per roll
+
+    return baseDamage * (1 + multiplier);
+}
 
     public bool IsEvenRoll()
     {
